1167a0d77550e759c0e5086379b0f29cdabb0f06
Marcelino Maita Vargas <maita.marcelino@utec.edu.pe>
2024-12-03 11:45:35 -0500

    redux

--- src/app/(header)/home/page.tsx
+++ src/app/(header)/home/page.tsx
@@ -4,8 +4,13 @@ import { Posts } from "@/components/posts";
 
 export default function Page() {
   return (
-    <main className="w-full">
+    <main className="w-full overflow-y-auto max-h-screen">
+       <div>
        <Posts></Posts> 
+       </div>
+       <div className="h-20">
+        
+       </div>
     </main>
   );
 }
